d045ca055c994a272c238e8769065cd2c72ebe13
Fahim Chowdhury <code@neurotechie.xyz>
2026-07-14 20:18:07 +0100

    Initialize Netlify Blob context

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,8 +1,10 @@
 import serverless from 'serverless-http';
+import { connectLambda } from '@netlify/blobs';
 
 let cachedHandler;
 
 export async function handler(event, context) {
+  connectLambda(event);
   if (!cachedHandler) {
     const mod = await import('../../server.js');
     await mod.initializeRuntime();
--- src/agency/appVersion.ts
+++ src/agency/appVersion.ts
@@ -1,4 +1,4 @@
-export const AGENCY_APP_VERSION = '1.1.0';
+export const AGENCY_APP_VERSION = '1.1.1';
 
 export function getAgencyBuildInfo() {
   return {
